2530295e7f253decfabecc56d88325f95b09f516
James Hough <59900120+jhok2013@users.noreply.github.com>
2024-07-30 11:53:29 -0500

    Added either to prelude

--- pythonix/prelude.py
+++ pythonix/prelude.py
@@ -46,6 +46,7 @@ import pythonix.dict_utils as dict_utils
 import pythonix.grammar as grammar
 import pythonix.prove as prove
 import pythonix.deq as deq
+import pythonix.either as either
 from pythonix.grammar import P, Piper
 from pythonix.fn import fn
 from pythonix.res import Ok, Err, Nil, q, Res, qe, ok, err, Opt, unpack
